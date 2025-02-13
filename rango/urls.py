c2bc4fede65631cad0f6de225f75524741ba61c6
Khaled Al Harbi <2961040a@student.gla.ac.uk>
2025-02-13 21:25:28 +0000

    chapter 6 test passed

--- rango/urls.py
+++ rango/urls.py
@@ -5,4 +5,8 @@ app_name = "rango"
 urlpatterns = [
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
+    path("view/", views.view, name="view"),
+    path(
+        "category/<slug:category_name_slug>/", views.show_category, name="show_category"
+    ),
 ]
