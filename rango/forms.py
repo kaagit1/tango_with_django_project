e3509b34eeefa4e0f2355f32fc87815da16718fc
Khaled Al Harbi <2961040a@student.gla.ac.uk>
2025-02-14 03:31:38 +0000

    chapter 9 test passed

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, User, UserProfile
 
 
 class CategoryForm(forms.ModelForm):
@@ -64,3 +64,24 @@ class PageForm(forms.ModelForm):
 
 # or specify the fields to include (don't include the category field).
 # fields = ('title', 'url', 'views')
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "password",
+        )
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = (
+            "website",
+            "picture",
+        )
